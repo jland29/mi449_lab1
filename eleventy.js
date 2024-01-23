23e9069e130726556638f1a57340824a735d5386
jland29 <lander10@msu.edu>
2024-01-23 01:06:36 -0500

    added javascript

---
+++ eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyConfig) {
+    // Output directory: _site
+  
+    // Copy `img/` to `_site/img`
+    eleventyConfig.addPassthroughCopy("images");
+  
+    // Copy `css/fonts/` to `_site/css/fonts`
+    // Keeps the same directory structure.
+    eleventyConfig.addPassthroughCopy("css");
+  };
